3dc6d7abdb1f4dde9d78e78dfe801aaae7687e00
Chris Khoury <chriskhoury32@gmail.com>
2022-12-02 17:30:06 -0500

    Modified i2s playback and transceiver

--- FPGA Project/i2s_transceiver.vhd
+++ FPGA Project/i2s_transceiver.vhd
@@ -1,112 +1,112 @@
 --------------------------------------------------------------------------------
 --
---   FileName:         i2s_transceiver.vhd
---   Dependencies:     none
---   Design Software:  Quartus Prime Version 17.0.0 Build 595 SJ Lite Edition
+--   filename:         i2s_transceiver.vhd
+--   dependencies:     none
+--   design software:  quartus prime version 17.0.0 build 595 sj lite edition
 --
---   HDL CODE IS PROVIDED "AS IS."  DIGI-KEY EXPRESSLY DISCLAIMS ANY
---   WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
---   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
---   PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
---   BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
---   DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
---   PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
---   BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
---   ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
+--   hdl code is provided "as is."  digi-key expressly disclaims any
+--   warranty of any kind, whether express or implied, including but not
+--   limited to, the implied warranties of merchantability, fitness for a
+--   particular purpose, or non-infringement. in no event shall digi-key
+--   be liable for any incidental, special, indirect or consequential
+--   damages, lost profits or lost data, harm to your equipment, cost of
+--   procurement of substitute goods, technology or services, any claims
+--   by third parties (including but not limited to any defense thereof),
+--   any claims for indemnity or contribution, or other similar costs.
 --
---   Version History
---   Version 1.0 03/29/2019 Scott Larson
---     Initial Public Release
+--   version history
+--   version 1.0 03/29/2019 scott larson
+--     initial public release
 -- 
 --------------------------------------------------------------------------------
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY i2s_transceiver IS
-  GENERIC(
-    mclk_sclk_ratio  :  INTEGER := 4;    --number of mclk periods per sclk period
-    sclk_ws_ratio    :  INTEGER := 64;   --number of sclk periods per word select period
-    d_width          :  INTEGER := 24);  --data width
-  PORT(
-    reset_n    :  IN   STD_LOGIC;                             --asynchronous active low reset
-    mclk       :  IN   STD_LOGIC;                             --master clock
-    sclk       :  OUT  STD_LOGIC;                             --serial clock (or bit clock)
-    ws         :  OUT  STD_LOGIC;                             --word select (or left-right clock)
-    sd_tx      :  OUT  STD_LOGIC;                             --serial data transmit
-    sd_rx      :  IN   STD_LOGIC;                             --serial data receive
-    l_data_tx  :  IN   STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --left channel data to transmit
-    r_data_tx  :  IN   STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --right channel data to transmit
-    l_data_rx  :  OUT  STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --left channel data received
-    r_data_rx  :  OUT  STD_LOGIC_VECTOR(d_width-1 DOWNTO 0)); --right channel data received
-END i2s_transceiver;
+entity i2s_transceiver is
+  generic(
+    mclk_sclk_ratio  :  integer := 4;    --number of mclk periods per sclk period
+    sclk_ws_ratio    :  integer := 64;   --number of sclk periods per word select period
+    d_width          :  integer := 24);  --data width
+  port(
+    reset_n    :  in   std_logic;                             --asynchronous active low reset
+    mclk       :  in   std_logic;                             --master clock
+    sclk       :  out  std_logic;                             --serial clock (or bit clock)
+    ws         :  out  std_logic;                             --word select (or left-right clock)
+    sd_tx      :  out  std_logic;                             --serial data transmit
+    sd_rx      :  in   std_logic;                             --serial data receive
+    l_data_tx  :  in   std_logic_vector(d_width-1 downto 0);  --left channel data to transmit
+    r_data_tx  :  in   std_logic_vector(d_width-1 downto 0);  --right channel data to transmit
+    l_data_rx  :  out  std_logic_vector(d_width-1 downto 0);  --left channel data received
+    r_data_rx  :  out  std_logic_vector(d_width-1 downto 0)); --right channel data received
+end i2s_transceiver;
 
-ARCHITECTURE logic OF i2s_transceiver IS
+architecture logic of i2s_transceiver is
 
-  SIGNAL sclk_int       :  STD_LOGIC := '0';                      --internal serial clock signal
-  SIGNAL ws_int         :  STD_LOGIC := '0';                      --internal word select signal
-  SIGNAL l_data_rx_int  :  STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --internal left channel rx data buffer
-  SIGNAL r_data_rx_int  :  STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --internal right channel rx data buffer
-  SIGNAL l_data_tx_int  :  STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --internal left channel tx data buffer
-  SIGNAL r_data_tx_int  :  STD_LOGIC_VECTOR(d_width-1 DOWNTO 0);  --internal right channel tx data buffer
+  signal sclk_int       :  std_logic := '0';                      --internal serial clock signal
+  signal ws_int         :  std_logic := '0';                      --internal word select signal
+  signal l_data_rx_int  :  std_logic_vector(d_width-1 downto 0);  --internal left channel rx data buffer
+  signal r_data_rx_int  :  std_logic_vector(d_width-1 downto 0);  --internal right channel rx data buffer
+  signal l_data_tx_int  :  std_logic_vector(d_width-1 downto 0);  --internal left channel tx data buffer
+  signal r_data_tx_int  :  std_logic_vector(d_width-1 downto 0);  --internal right channel tx data buffer
    
-BEGIN  
+begin  
   
-  PROCESS(mclk, reset_n)
-    VARIABLE sclk_cnt  :  INTEGER := 0;  --counter of master clocks during half period of serial clock
-    VARIABLE ws_cnt    :  INTEGER := 0;  --counter of serial clock toggles during half period of word select
-  BEGIN
+  process(mclk, reset_n)
+    variable sclk_cnt  :  integer := 0;  --counter of master clocks during half period of serial clock
+    variable ws_cnt    :  integer := 0;  --counter of serial clock toggles during half period of word select
+  begin
     
-    IF(reset_n = '0') THEN                                           --asynchronous reset
+    if(reset_n = '0') then                                           --asynchronous reset
       sclk_cnt := 0;                                                   --clear mclk/sclk counter
       ws_cnt := 0;                                                     --clear sclk/ws counter
       sclk_int <= '0';                                                 --clear serial clock signal
       ws_int <= '0';                                                   --clear word select signal
-      l_data_rx_int <= (OTHERS => '0');                                --clear internal left channel rx data buffer
-      r_data_rx_int <= (OTHERS => '0');                                --clear internal right channel rx data buffer
-      l_data_tx_int <= (OTHERS => '0');                                --clear internal left channel tx data buffer
-      r_data_tx_int <= (OTHERS => '0');                                --clear internal right channel tx data buffer
+      l_data_rx_int <= (others => '0');                                --clear internal left channel rx data buffer
+      r_data_rx_int <= (others => '0');                                --clear internal right channel rx data buffer
+      l_data_tx_int <= (others => '0');                                --clear internal left channel tx data buffer
+      r_data_tx_int <= (others => '0');                                --clear internal right channel tx data buffer
       sd_tx <= '0';                                                    --clear serial data transmit output
-      l_data_rx <= (OTHERS => '0');                                    --clear left channel received data output
-      r_data_rx <= (OTHERS => '0');                                    --clear right channel received data output
-    ELSIF(mclk'EVENT AND mclk = '1') THEN                            --master clock rising edge
-      IF(sclk_cnt < mclk_sclk_ratio/2-1) THEN                          --less than half period of sclk
+      l_data_rx <= (others => '0');                                    --clear left channel received data output
+      r_data_rx <= (others => '0');                                    --clear right channel received data output
+    elsif(mclk'event and mclk = '1') then                            --master clock rising edge
+      if(sclk_cnt < mclk_sclk_ratio/2-1) then                          --less than half period of sclk
         sclk_cnt := sclk_cnt + 1;                                        --increment mclk/sclk counter
-      ELSE                                                             --half period of sclk
+      else                                                             --half period of sclk
         sclk_cnt := 0;                                                   --reset mclk/sclk counter
-        sclk_int <= NOT sclk_int;                                        --toggle serial clock
-        IF(ws_cnt < sclk_ws_ratio-1) THEN                                --less than half period of ws
+        sclk_int <= not sclk_int;                                        --toggle serial clock
+        if(ws_cnt < sclk_ws_ratio-1) then                                --less than half period of ws
           ws_cnt := ws_cnt + 1;                                            --increment sclk/ws counter
-          IF(sclk_int = '0' AND ws_cnt > 1 AND ws_cnt < d_width*2+2) THEN  --rising edge of sclk during data word
-            IF(ws_int = '1') THEN                                            --right channel
-              r_data_rx_int <= r_data_rx_int(d_width-2 DOWNTO 0) & sd_rx;      --shift data bit into right channel rx data buffer
-            ELSE                                                             --left channel
-              l_data_rx_int <= l_data_rx_int(d_width-2 DOWNTO 0) & sd_rx;      --shift data bit into left channel rx data buffer
-            END IF;
-          END IF;
-          IF(sclk_int = '1' AND ws_cnt < d_width*2+3) THEN                 --falling edge of sclk during data word
-            IF(ws_int = '1') THEN                                            --right channel
+          if(sclk_int = '0' and ws_cnt > 1 and ws_cnt < d_width*2+2) then  --rising edge of sclk during data word
+            if(ws_int = '1') then                                            --right channel
+              r_data_rx_int <= r_data_rx_int(d_width-2 downto 0) & sd_rx;      --shift data bit into right channel rx data buffer
+            else                                                             --left channel
+              l_data_rx_int <= l_data_rx_int(d_width-2 downto 0) & sd_rx;      --shift data bit into left channel rx data buffer
+            end if;
+          end if;
+          if(sclk_int = '1' and ws_cnt < d_width*2+3) then                 --falling edge of sclk during data word
+            if(ws_int = '1') then                                            --right channel
               sd_tx <= r_data_tx_int(d_width-1);                               --transmit serial data bit 
-              r_data_tx_int <= r_data_tx_int(d_width-2 DOWNTO 0) & '0';        --shift data of right channel tx data buffer
-            ELSE                                                             --left channel
+              r_data_tx_int <= r_data_tx_int(d_width-2 downto 0) & '0';        --shift data of right channel tx data buffer
+            else                                                             --left channel
               sd_tx <= l_data_tx_int(d_width-1);                               --transmit serial data bit
-              l_data_tx_int <= l_data_tx_int(d_width-2 DOWNTO 0) & '0';        --shift data of left channel tx data buffer
-            END IF;
-          END IF;        
-        ELSE                                                            --half period of ws
+              l_data_tx_int <= l_data_tx_int(d_width-2 downto 0) & '0';        --shift data of left channel tx data buffer
+            end if;
+          end if;        
+        else                                                            --half period of ws
           ws_cnt := 0;                                                    --reset sclk/ws counter
-          ws_int <= NOT ws_int;                                           --toggle word select
+          ws_int <= not ws_int;                                           --toggle word select
           r_data_rx <= r_data_rx_int;                                     --output right channel received data
           l_data_rx <= l_data_rx_int;                                     --output left channel received data
           r_data_tx_int <= r_data_tx;                                     --latch in right channel data to transmit
           l_data_tx_int <= l_data_tx;                                     --latch in left channel data to transmit
-        END IF;
-      END IF;
-    END IF;    
-  END PROCESS;
+        end if;
+      end if;
+    end if;    
+  end process;
   
   sclk <= sclk_int;  --output serial clock
   ws <= ws_int;      --output word select
   
-END logic;
+end logic;
     
