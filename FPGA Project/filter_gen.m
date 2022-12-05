46f39a6e8fd861342d7709939db7a93c9c4ad924
BenjaminPromisel <bzpromisel@gmail.com>
2022-12-04 19:07:36 -0500

    Updated impulse responses

--- FPGA Project/filter_gen.m
+++ FPGA Project/filter_gen.m
@@ -5,26 +5,28 @@ cutoffs = 100 * 1.4.^(0:15);
 table = zeros(1, 32*num_samples);
 
 for i = 1:length(cutoffs)
-    h_low = find_impulse(cutoffs(i), fs, num_samples);
-    h_high = [1 zeros(1,length(h_low)-1)] - h_low;
+    [h_low, h_high] = find_impulses(cutoffs(i), fs, num_samples);
     
     data_low = arrayfun(@float_to_int, h_low);
     data_high = arrayfun(@float_to_int, h_high);
     
     start = num_samples*(i-1)+1;
     stop = num_samples*i;
-    table(start:stop) = data_low;
-    table((start:stop)+16*num_samples) = data_high;
+    table(start:stop) = fliplr(data_low);
+    table((start:stop)+16*num_samples) = fliplr(data_high);
 end
 
-mk_rom(table, 'filter_table1', 'signed');
+mk_rom(table, 'filter_table', 'signed');
 
-function h = find_impulse(cutoff, fs, num_samples)
-    pass_width = floor(cutoff / fs * num_samples);
-    H = zeros(1, num_samples);
+function [h_low, h_high] = find_impulses(cutoff, fs, num_samples)
+    pass_width = floor(cutoff / fs * 2*num_samples);
+    H = zeros(1, 2*num_samples);
     H(1:pass_width) = 1;
     H((end-(pass_width-2)):end) = 1;
-    h = ifft(H);
+    h_low = ifft(H);
+    h_low = h_low(1:num_samples);
+    h_high = ifft(1-H);
+    h_high = h_high(1:num_samples);
 end
 
 function n = float_to_int(x)
